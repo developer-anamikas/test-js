af0bfb1047365db6a17044069fdf1ff61733a97b
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-05 18:21:43 +0000

    Learnt to use var , const and let

--- 1_basics/test.js
+++ 1_basics/test.js
@@ -1,2 +1,24 @@
 console.log("ANAMIKA HERE!")
 console.log("B.TECH")
+
+// LET , VAR & CONST
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log([accountId,accountEmail,accountPassword,accountState,accountCity]);
