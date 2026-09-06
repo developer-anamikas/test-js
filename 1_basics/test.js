724799e753b31ec33edd43574a4f9d7308513bbc
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-06 17:21:21 +0000

    conversion of datatypes

--- 1_basics/test.js
+++ 1_basics/test.js
@@ -1,62 +1,150 @@
-console.log("ANAMIKA HERE!")
-console.log("B.TECH")
+// console.log("ANAMIKA HERE!")
+// console.log("B.TECH")
 
-// LET , VAR & CONST
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+// // LET , VAR & CONST
+// const accountId = 144553
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
 
-// accountId = 2 // not allowed
+// // accountId = 2 // not allowed
 
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
 
-console.log(accountId);
+// console.log(accountId);
 
-/*
-prefer not to use var
-because of issue in block scope and functional scope
-*/
+// /*
+// prefer not to use var
+// because of issue in block scope and functional scope
+// */
 
-console.log([accountId,accountEmail,accountPassword,accountState,accountCity]);
-
-
-// DATATYPES
-
-"use strict"; // treat all JS code as newer version
-
-// alert( 3 + 3) // we are using nodejs, not browser
-
-console.log(3
-    +
-    3) // code readability should be high
-
-console.log("Hitesh")
-
-console.log("Hitesh")
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state = null
-let state1;
-
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined =>
-// undefined =>
-// symbol => unique
-// object => null
-// undefined => undefined displayed
+// console.log([accountId,accountEmail,accountPassword,accountState,accountCity]);
 
 
-console.log(typeof "hitesh");
-console.log(typeof age);
-console.log(typeof state);
-console.log(typeof state1);
+// // DATATYPES
+
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3
+//     +
+//     3) // code readability should be high
+
+// console.log("Hitesh")
+
+// console.log("Hitesh")
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state = null
+// let state1;
+
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined =>
+// // undefined =>
+// // symbol => unique
+// // object => null
+// // undefined => undefined displayed
+
+
+// console.log(typeof "hitesh");
+// console.log(typeof age);
+// console.log(typeof state);
+// console.log(typeof state1);
+
+
+
+
+// //DATATYPES CONVERSION
+
+//         /* name -> number types */
+// let name ="Anamika"
+// let convertToNum = Number(name)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// name = 1234
+// convertToNum = Number(name)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// name = ""
+// convertToNum = Number(name)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// name=null
+// convertToNum = Number(name)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// name="abc888"
+// convertToNum = Number(name)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// let name1=undefined;
+// convertToNum = Number(name1)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+// let name2=true
+// convertToNum = Number(name2)
+// console.log(typeof convertToNum)
+// console.log(convertToNum)
+
+//     /* "Anamika" → NaN
+//     1234 → 1234
+//     "" → 0
+//     null → 0
+//     "abc888" → NaN
+//     undefined → NaN
+//     true → 1
+//     false → 0*/
+
+// let value1=1
+// let convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+// value1=2
+// convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+
+// value1=""
+// convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+// value1=null
+// convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+// value1="Anamika"
+// convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+// value1=0
+// convertToBoolean= Boolean(value1)
+// console.log(convertToBoolean)
+
+
+// /* 1 → true
+// 2 → true
+// "" → false
+// null → false
+// "Anamika" → true
+// 0 → false */
+
+// let value2=33
+// let convetToString= String(value2)
+// console.log(typeof convetToString)
+// console.log(convetToString)
