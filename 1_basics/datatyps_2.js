8362e79fc2d2e04283c9190ea4acd5e9423d0aa2
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-07 16:02:33 +0000

    Datatypes summary

---
+++ 1_basics/datatyps_2.js
@@ -0,0 +1,31 @@
+// /********DATATYPES********* */
+
+// //In JavaScript, data types are officially categorized into two groups—Primitive and Non-primitive
+// //  (Reference) types—based on how they are stored in memory and accessed by the program 
+// // Primitive types (Call by value): When you copy these, you get an actual copy of the data, not a reference to the original memory space.
+// //  Changes to the copy do not affect the original 
+// // Non-primitive/Reference types: These provide a direct reference to the memory location where
+// //  the data is stored
+// const id1= Symbol("123")
+// const id2= Symbol("123")
+// console.log(id1==id2)
+
+// const bigInt=999999999999999999n 
+// console.log(typeof bigInt);
+
+// const myarray=["Shaktiman", "naagraj","doga"]  //array declared
+// let myobj={
+//     name:"Anamika",   // comma , notice , obj dclared
+//     age:20,
+// }
+
+// const funcvariable = function myFunction(){
+//     console.log("helloo!!!!");
+// }
+// console.log(typeof myarray);
+// console.log(typeof myobj);
+// console.log(typeof funcvariable);
+
+
+
+
