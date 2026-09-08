7f83860293c4286b2c11b401b599b7029ee1d177
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-08 15:31:23 +0000

    Learnt more About Arrays

---
+++ 2_basics/02_Arrays.js
@@ -0,0 +1,32 @@
+// /***************ARRAY PART2***************/
+
+
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+// const dc_heros = ["superman", "flash", "batman"]
+// const clg=["MSIT","IEM"]
+
+// marvel_heros.push(dc_heros);    //push value and original arr affected
+// console.log(marvel_heros);          //arr are not properly merged like this , arr within array
+// console.log(marvel_heros[3][0]);
+
+// const myconcat= marvel_heros.concat(dc_heros);      //tomerge 2 array into one
+// console.log(myconcat);
+// const myconcat1= [...marvel_heros,...dc_heros,...clg]  //multiple concat
+// console.log(myconcat1);
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(Infinity)   //combines sub-arrays to arrays untilla particular depth = infinity choosen , autmatially detects depth
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Hitesh"));              //to check whther it is array or not EG - Hitesh
+// console.log(Array.from("Hitesh"));                 //change string , objs , etc to Array
+// console.log(Array.from({name:"Anamika"}));          //changes onj to Array but cant do coz is that you didnt specify that key or value would be Array?
+
+// const score1=100
+// const score2=200
+// const score3=300
+
+// console.log(Array.of(score1,score2,score3));
+
+
