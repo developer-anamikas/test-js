00854808690df9bb6b3a13bc44d7926763c5844f
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-11 06:02:46 +0000

    Learnt about Scope

---
+++ 3_basics/02_scope.js
@@ -0,0 +1,18 @@
+// var c=300       //global scope -> for var the value changes to c=30
+// let a=100      //global scope -> the global variable declared using let ,const doesn't change when declared globally   
+// const b=200;
+
+// if(true){
+//     let a= 10
+//     const b=20
+//     console.log(b);
+//     var c=30            //Dont use var
+// }
+
+// console.log(a);   //show error coz its scoe is within if {} block
+// console.log(b);  //show error coz its scoe is within if {} block
+// console.log(c);     //scop doesn't work
+
+
+/**********GLOBALSCOPE WORKS DIFFERENTLY IN NODE.JS ENVIORNMENT & BROWSER**********/
+
