426bffc1587c632ad351301506dea6fa820bfac8
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-11 07:24:49 +0000

    More about Scope

--- 3_basics/02_scope.js
+++ 3_basics/02_scope.js
@@ -14,5 +14,46 @@
 // console.log(c);     //scop doesn't work
 
 
+// const z=10          //globally declared
+// if(true){
+//     console.log(z);   
+// }
+
 /**********GLOBALSCOPE WORKS DIFFERENTLY IN NODE.JS ENVIORNMENT & BROWSER**********/
 
+// function one(){
+//     const username ="hitesh"
+//     function two(){
+//         const website ="hitesh"
+//         console.log(username);              
+//     }
+//     // console.log(website);
+
+//     // two()                    //Because two() is inside the scope of one(), so two() can access variables created in one().
+// }
+// one()                   //only one() is executed not two() , you need to call it using two()
+
+
+// if(true){
+//     const username ="hitesh"
+//     if(username==="hitesh"){
+//         const website="youtube"
+//         console.log(username + " " + website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(username);
+
+// /*****************INTERESTING THING TO REMEMBER******************/
+
+// console.log(one(1));
+
+// function one(num){                   //here function is declared directly 
+//     return num +1
+// }
+
+// two(1)                                  //wont work 
+// const two=function (num){               //here function is declared with the variable
+//     return num +1
+// }
+// console.log(two(1));
