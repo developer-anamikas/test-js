9ec74a2e17641a9568601a043eba0a0cb70316b2
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-22 14:21:45 +0000

    continued with control Flow and started off with iterations

--- 4_controlFlow/one.js
+++ 4_controlFlow/one.js
@@ -43,5 +43,24 @@
 //     console.log("User logged in");
 // }
 
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "march":
+    console.log("march");
+    break;
+  case "april":
+    console.log("april");
+    break;
+  default:
+    console.log("default case match");
+    break;
+
+}
+
 
 
