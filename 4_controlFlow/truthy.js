2e843b7d71b6b877b19f57160949a3806b5b8354
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-22 03:09:33 +0000

    Control Flow part-1

---
+++ 4_controlFlow/truthy.js
@@ -0,0 +1,35 @@
+// const userEmail = []
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// false == 0
+// true
+// false == ''
+// true
+// 0 == ''
+// true
+
+// // Nullish Coalescing Operator (??): null undefined
+
+// let val1;       
+// val1 = 5 ?? 10          // first value 5 print
+// val1 = null ?? 10       // 10 displayed
+// val1 = undefined ?? 15      //15 displayed
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// // Ternary Operator
+
+// // condition ? true : false
+
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
