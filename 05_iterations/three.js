3b116b6ab864ddc24274e97b7e7d178e3e6dbad1
Anamika Sahu <anamikasahu.cseiot15@gmail.com>
2026-09-23 03:36:53 +0000

    Learnt About Maps & Arrays

---
+++ 05_iterations/three.js
@@ -0,0 +1,35 @@
+// // for of
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr =[1,2,3,4,5]
+
+// for (const num of arr) {    //array , obj , string also can be used
+//     console.log(num);
+// }
+
+// const greetings = "HELLO WORLD!"
+// for(const greet of greetings){
+//     console.log(`Each char is ${greet}`)
+// }
+
+// // Maps :-colection of unique values
+
+// const map = new Map()
+// map.set('IN',"INDIA")                    //map is object itself
+// map.set('USA',"USA")
+// map.set('Fr',"France")
+// map.set('IN',"INDIA")    
+// // console.log(map)
+
+// for (const key of map) {                //returns Array
+//     console.log(key);
+// }
+// console.log(map)
+
+// for (const [key, value] of map) {            // to get values separately
+//     console.log(key, ':-', value);
+// }
+
+
